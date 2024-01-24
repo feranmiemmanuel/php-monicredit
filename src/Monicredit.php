35d1cd61c227299799a91264e3f7ebce113a68c4
feranmi <feranmiolasunkanmi91@gmail.com>
2024-01-24 12:41:19 +0100

    working on get and post requests

--- src/Monicredit.php
+++ src/Monicredit.php
@@ -13,23 +13,54 @@ use GuzzleHttp\Client;
  {
     protected $publicKey;
     protected $privateKey;
+    protected $revenueHeadId;
+    protected $customer;
+    protected $settlementBankId;
     protected $response;
-    protected $BaseUrlType;
+    protected $baseUrlType;
 
-    public function client($BaseUrlType)
+    public function client(string $baseUrlType)
     {
-        return new Client(['base_uri' => $this->getBaseURI($BaseUrlType)]);
+        return new Client(['base_uri' => $this->getBaseURI($baseUrlType)]);
     }
 
-    public function getBaseURI($BaseUrlType)
+    public function getBaseURI(string $baseUrlType)
     {
-        switch ($BaseUrlType){
+        switch ($baseUrlType){
             case 'DEMO':
-                return 'https://demo.backend.monicredit.com/api/v1/';
+                return 'https://demo.backend.monicredit.com/';
             case 'LIVE':
-                return 'https://live.backend.monicredit.com/api/v1/';
+                return 'https://live.backend.monicredit.com/';
             default:
                 break;
         }
+    }
+
+    protected function post(string $path, array $payload, string $baseUrlType)
+    {
+        $response = $this->client($baseUrlType)->post(
+            'api/v1/' . $path,
+            [
+                'headers' => ['Content-Type' => 'application/json'],
+                'json' => $payload,
+            ]
+        );
+
+        $this->response = $response;
+        return json_decode($response->getBody(), true);
+    }
+
+    protected function get(string $path, array $payload, string $baseUrlType)
+    {
+        $response = $this->client($baseUrlType)->get(
+            'api/v1/' . $path,
+            [
+                'headers' => ['Content-Type' => 'application/json'],
+                'query' => $payload,
+            ]
+        );
+
+        $this->response = $response;
+        return json_decode($response->getBody(), true);
     }
  }
